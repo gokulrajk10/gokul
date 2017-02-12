1402cc9a5e0ae2b65749f880e5d5788a049d7358
gokulrajk10 <kgokulraj1994@gmail.com>
2017-02-12 11:46:21 +0530

    Add files via upload

--- Vowelrconst.java
+++ Vowelrconst.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Scanner;
-public class Vowelrconst
+public class vowelrconst
 {
     public static void main(String[] args) throws Exception 
     {
