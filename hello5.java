5313e7f2271b7f3387153c059a5d4ed2043e0df1
gokulrajk10 <kgokulraj1994@gmail.com>
2017-02-15 06:25:09 +0530

    Update hello5.java

--- hello5.java
+++ hello5.java
@@ -1,9 +1,9 @@
 import java.io.*;
 class hello5 
-{
+{	string a="hello";
 	public static void main(String args[])
 	{
 	for(int i=0;i<5;i++)
-	System.out.println("hello");
+	System.out.println(a);
 	}
 }
